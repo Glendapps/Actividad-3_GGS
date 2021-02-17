ae2def0158f725cd382078da5ada520dff515e45
Glendapps <78885940+Glendapps@users.noreply.github.com>
2021-02-17 12:41:17 -0600

    Add files via upload

---
+++ Actividad 3_GGS.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+var numEn = 100
+let numEn2: Int = 150
+
+var numDec = 90.9
+let Dec:Float = 99.9
+
+var cadenaT = "Bakugou"
+let cadenaT2 = "Deku"
+
+var Zelda = (45)
+print ("Legend of Zelda tiene \(Zelda) juegos")
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var diasSemana: Dictionary = [1: "Lunes", 2: "Martes", 3: "Miércoles", 4: "Jueves", 5: "Viernes", 6: "Sábado", 7: "Domingo"]
+
+print (diasSemana);
+
+
